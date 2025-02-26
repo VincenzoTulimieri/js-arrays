c7fc7d9289692967a58be0ec7d28216a9437f72e
VincenzoTulimieri <vincenzo22tulimieri@gmail.com>
2025-02-26 16:30:49 +0100

    fix: correzione punto 5 e 6

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -19,6 +19,7 @@ console.log(fourthTeacher)
  teachers[4] = 'Patrick'
  console.log(teachers[4])
  console.log(teachers)
+ 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
  const lastTeacher = teachers.pop();
  console.log(lastTeacher)
